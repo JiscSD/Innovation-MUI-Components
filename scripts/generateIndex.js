7da0009f1524be9f73888ebf9e0d099d6822c93a
Kiran Joshi <kiran.joshi@jisc.ac.uk>
2020-06-25 09:36:07 +0100

    Add script to auto-generate index

---
+++ scripts/generateIndex.js
@@ -0,0 +1,22 @@
+const { readdirSync, writeFileSync } = require('fs');
+
+const findDirectories = (source) =>
+    readdirSync(source, { withFileTypes: true })
+        .filter((dirent) => dirent.isDirectory())
+        .map((dirent) => dirent.name);
+
+const exportStatement = (components) => `export { theme, ${components.join(', ')} };`;
+
+const importStatements = (components) =>
+    components.map((component) => `import ${component} from './components/${component}';`).join('\n');
+
+const componentNames = findDirectories('src/components');
+
+const indexJs = `import theme from './theme.js';
+
+${importStatements(componentNames)}
+
+${exportStatement(componentNames)}
+`;
+
+writeFileSync('./src/index.js', indexJs);
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import CoronaBanner from './components/CoronaBanner';
 import JiscBanner from './components/JiscBanner';
 import JiscBoombox from './components/JiscBoombox';
 import JiscButton from './components/JiscButton';
-import JiscLogo from './components/JiscLogo';
+import JiscExpansionPanel from './components/JiscExpansionPanel';
+import JiscFeatureBox from './components/JiscFeatureBox';
 import JiscInfoBox from './components/JiscInfoBox';
+import JiscLogo from './components/JiscLogo';
+import JiscQuoteBox from './components/JiscQuoteBox';
 import JiscTopBox from './components/JiscTopBox';
 
-export { theme, CoronaBanner, JiscBanner, JiscBoombox, JiscButton, JiscLogo, JiscInfoBox, JiscTopBox };
+export { theme, CoronaBanner, JiscBanner, JiscBoombox, JiscButton, JiscExpansionPanel, JiscFeatureBox, JiscInfoBox, JiscLogo, JiscQuoteBox, JiscTopBox };
