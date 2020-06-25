0c2fbde43735dc3a5b743f011fd241d44bd26e70
nathan-at-jisc <nathan.sainsbury@jisc.ac.uk>
2020-06-25 14:00:33 +0100

    updating index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import theme from './theme.js';
 
 import JiscBanner from './components/JiscBanner';
-import CoronaBanner from './components/CoronaBanner';
 import JiscBoombox from './components/JiscBoombox';
 import JiscButton from './components/JiscButton';
+import JiscCoronaBanner from './components/JiscCoronaBanner';
 import JiscExpansionPanel from './components/JiscExpansionPanel';
 import JiscFeatureBox from './components/JiscFeatureBox';
 import JiscInfoBox from './components/JiscInfoBox';
 import JiscLogo from './components/JiscLogo';
 import JiscQuoteBox from './components/JiscQuoteBox';
+import JiscTitleBanner from './components/JiscTitleBanner';
 import JiscTopBox from './components/JiscTopBox';
 
-export {
-    theme,
-    CoronaBanner,
-    JiscBanner,
-    JiscBoombox,
-    JiscButton,
-    JiscExpansionPanel,
-    JiscFeatureBox,
-    JiscInfoBox,
-    JiscLogo,
-    JiscQuoteBox,
-    JiscTopBox
-};
+export { theme, JiscBanner, JiscBoombox, JiscButton, JiscCoronaBanner, JiscExpansionPanel, JiscFeatureBox, JiscInfoBox, JiscLogo, JiscQuoteBox, JiscTitleBanner, JiscTopBox };
